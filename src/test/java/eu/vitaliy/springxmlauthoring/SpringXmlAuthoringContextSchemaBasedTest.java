3e6294d97924c253166773170c60b2cf330ee4c3
VitaliyOliynyk <xaoc00@gmail.com>
2014-10-26 21:18:37 +0100

    step-02 konfiguracja tabview na podstawie schama

---
+++ src/test/java/eu/vitaliy/springxmlauthoring/SpringXmlAuthoringContextSchemaBasedTest.java
@@ -0,0 +1,64 @@
+package eu.vitaliy.springxmlauthoring;
+
+import com.cc.controller.SimpleViewController;
+import com.cc.tabs.TabGroup;
+import com.cc.tabs.TabView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+@ContextConfiguration("classpath:/META-INF/application-context-root-schama-based.xml")
+public class SpringXmlAuthoringContextSchemaBasedTest extends AbstractTestNGSpringContextTests {
+
+    private static final int COUNT_OF_TABS = 3;
+    @Autowired
+    private SimpleViewController tab1Controller;
+
+    @Autowired
+    private SimpleViewController tab2Controller;
+
+    @Autowired
+    private SimpleViewController tab3Controller;
+
+    @Autowired
+    private TabView tab1;
+
+    @Autowired
+    private TabView tab2;
+
+    @Autowired
+    private TabView tab3;
+
+    @Autowired
+    private TabGroup tabGroup;
+
+    @Test
+    public void applicationContextTest() {
+        checkControllers(tab1Controller, tab2Controller, tab3Controller);
+        checkTabs(tab1, tab2, tab3);
+        checkTabGroup(tabGroup);
+    }
+
+    private void checkTabGroup(TabGroup tabGroup) {
+        assertThat(tabGroup).isNotNull();
+        assertThat(tabGroup.getTabs()).hasSize(COUNT_OF_TABS);
+    }
+
+    private void checkTabs(TabView ...  tabs) {
+        for (TabView tab : tabs) {
+            assertThat(tab).isNotNull();
+            assertThat(tab.getController()).isNotNull();
+        }
+
+    }
+
+    private void checkControllers(SimpleViewController... controllers) {
+        for (SimpleViewController controller : controllers) {
+            assertThat(controller).isNotNull();
+        }
+    }
+
+}
