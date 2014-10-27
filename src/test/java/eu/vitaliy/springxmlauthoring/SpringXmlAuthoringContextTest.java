23a7387d90f7823809539b10ecf93db493b6740d
VitaliyOliynyk <xaoc00@gmail.com>
2014-10-27 20:53:29 +0100

    refaktoring

--- src/test/java/eu/vitaliy/springxmlauthoring/SpringXmlAuthoringContextTest.java
+++ src/test/java/eu/vitaliy/springxmlauthoring/SpringXmlAuthoringContextTest.java
@@ -10,55 +10,5 @@ import org.testng.annotations.Test;
 import static org.fest.assertions.Assertions.*;
 
 @ContextConfiguration("classpath:/META-INF/application-context-root.xml")
-public class SpringXmlAuthoringContextTest extends AbstractTestNGSpringContextTests {
-
-    private static final int COUNT_OF_TABS = 3;
-    @Autowired
-    private SimpleViewController tab1Controller;
-
-    @Autowired
-    private SimpleViewController tab2Controller;
-
-    @Autowired
-    private SimpleViewController tab3Controller;
-
-    @Autowired
-    private TabView tab1;
-
-    @Autowired
-    private TabView tab2;
-
-    @Autowired
-    private TabView tab3;
-
-    @Autowired
-    private TabGroup tabGroup;
-
-    @Test
-    public void applicationContextTest() {
-        checkControllers(tab1Controller, tab2Controller, tab3Controller);
-        checkTabs(tab1, tab2, tab3);
-        checkTabGroup(tabGroup);
-    }
-
-    private void checkTabGroup(TabGroup tabGroup) {
-        assertThat(tabGroup).isNotNull();
-        assertThat(tabGroup.getTabs()).hasSize(COUNT_OF_TABS);
-        assertThat(tabGroup.getTabs()).contains(tab1, tab2, tab3);
-    }
-
-    private void checkTabs(TabView ...  tabs) {
-        for (TabView tab : tabs) {
-            assertThat(tab).isNotNull();
-            assertThat(tab.getController()).isNotNull();
-        }
-
-    }
-
-    private void checkControllers(SimpleViewController... controllers) {
-        for (SimpleViewController controller : controllers) {
-            assertThat(controller).isNotNull();
-        }
-    }
-
+public class SpringXmlAuthoringContextTest extends AbstractSpringXmlAuthoringContextTest {
 }
